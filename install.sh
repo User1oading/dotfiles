c9183e177aa63cc3acab6acfd5877ec9befd31f2
User1oading <71985808+User1oading@users.noreply.github.com>
2022-08-30 11:31:47 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
-sudo pacman -S discord steam gimp alacritty feh
-yay -S minecraft-launcher ani-cli gdlauncher autotiling unity-hub lutris cmatrix-git zoom betterdiscordctl 
+sudo pacman -S discord steam gimp alacritty feh obs-studio wine
+yay -S minecraft-launcher ani-cli gdlauncher autotiling unity-hub lutris cmatrix-git zoom nerd-fonts-complete
+#yay -S betterdiscordctl mangohud etcher-bin cemu-git teamviewer
