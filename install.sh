2b3ec3b6104fb53ff42d58ff6013042445aead4a
Loading <71985808+User1oading@users.noreply.github.com>
2026-07-21 00:50:40 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,2 @@
-sudo pacman -S discord steam gimp alacritty feh obs-studio wine
-yay -S minecraft-launcher ani-cli gdlauncher autotiling unity-hub lutris cmatrix-git zoom nerd-fonts-complete
-#yay -S betterdiscordctl mangohud etcher-bin cemu-git teamviewer
+sudo pacman -S discord steam kitty feh
+yay -S autotiling unity-hub nerd-fonts-complete
